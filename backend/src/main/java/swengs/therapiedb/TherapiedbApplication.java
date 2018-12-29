a35563efabec45e343b814558edbbd64d7d70ed4
Celyca <Patrik.Rauche@gmail.com>
2018-12-29 16:05:00 +0100

    first commit

---
+++ backend/src/main/java/swengs/therapiedb/TherapiedbApplication.java
@@ -0,0 +1,14 @@
+package swengs.therapiedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapiedbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TherapiedbApplication.class, args);
+    }
+
+}
+
