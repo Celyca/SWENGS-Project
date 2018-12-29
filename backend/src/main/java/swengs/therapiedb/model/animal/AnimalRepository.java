9d714219475b2137256e4f8bd568a20e5b3c3ea9
Celyca <Patrik.Rauche@gmail.com>
2018-12-29 17:15:56 +0100

    [Backend] Update Models

--- backend/src/main/java/swengs/therapiedb/model/animal/AnimalRepository.java
+++ backend/src/main/java/swengs/therapiedb/model/animal/AnimalRepository.java
@@ -2,7 +2,6 @@ package swengs.therapiedb.model.animal;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import therapie.db.model.user.UserImage;
 
 @RepositoryRestResource
 public interface AnimalRepository extends PagingAndSortingRepository<Animal,Long> {
