a6d965fd74c20972022cad72b1832d58288cddc5
cookiemonster25 <huettersue@gmail.com>
2019-01-16 19:11:12 +0100

    [Backend] Data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+/*   TIERARTEN    */
+INSERT INTO species (name, version) VALUES ("Hund", 0);
+INSERT INTO species (name, version) VALUES ("Katze", 0);
+INSERT INTO species (name, version) VALUES ("Kleintier", 0);
+
+/*   ORTE    */
+
+INSERT INTO location (address, city, email, `name`, phone, version, zip) VALUES ("Test1", "Graz", "G@wd.gmail.com", "WD Graz", "0664/123456789", 0, 8010);
+INSERT INTO location (address, city, email, `name`, phone, version, zip) VALUES ("Test2", "Wien", "W@wd.gmail.com", "WD Wien", "0664/123456789", 0, 5020);
+INSERT INTO location (address, city, email, `name`, phone, version, zip) VALUES ("Test3", "Salzburg", "S@wd.gmail.com", "WD Salzburg", "0664/123456789", 0, 1030);
+
