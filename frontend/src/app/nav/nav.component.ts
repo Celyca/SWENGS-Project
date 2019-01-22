8bc56620d272f97e92d558c1885a50611776d8d8
Celyca <Patrik.Rauche@gmail.com>
2019-01-22 19:07:15 +0100

    [Frontend] Dropdown

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -3,6 +3,9 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {UserService} from '../service/user.service';
+import {User} from '../api/user';
+import {UserProfileService} from '../service/user-profile.service';
+import {UserProfile} from '../api/profile';
 
 @Component({
   selector: 'app-nav',
@@ -13,23 +16,41 @@ export class NavComponent implements OnInit {
 
   clicked: boolean;
   isLoggedIn: boolean;
+  user: User;
+  userProfile: UserProfile;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private userService: UserService, private breakpointObserver: BreakpointObserver) {
+  constructor(private userService: UserService,
+              private breakpointObserver: BreakpointObserver,
+              private userProfileService: UserProfileService) {
     this.clicked = this.clicked === undefined ? false : true;
   }
 
-  ngOnInit() { this.isLoggedIn = this.userService.isLoggedIn;
+  ngOnInit() {
+    this.isLoggedIn = this.userService.isLoggedIn;
     this.userService.loggedInChange.subscribe((isLoggedIn) => {
       this.isLoggedIn = isLoggedIn;
+      if (this.isLoggedIn) {
+        this.getUser();
+      }
     });
+    this.getUser();
   }
 
-
+  getUser() {
+    if (this.isLoggedIn) {
+      this.userService.getAuth().subscribe((user: any) => {
+        this.user = user;
+        this.userProfileService.getById(this.user.id).subscribe((userProfile: any) => {
+          this.userProfile = userProfile;
+        });
+      });
+    }
+  }
   logout() {
     this.userService.logout();
   }
