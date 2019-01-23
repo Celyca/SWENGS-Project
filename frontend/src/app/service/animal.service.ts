e255650a8901e5f0516e049d14f2a8d4bc138fed
Celyca <Patrik.Rauche@gmail.com>
2019-01-23 16:22:56 +0100

    [Frontend] Install Toastr

--- frontend/src/app/service/animal.service.ts
+++ frontend/src/app/service/animal.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Animal} from '../api/animal';
-import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
